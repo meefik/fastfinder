57caa3c6d0f711bedf26808e7e7ff2ec87545a52
Anton Skshidlevsky <meefik@gmail.com>
2023-02-04 14:51:26 +0100

    Auto restart when ssl changes

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,10 +5,18 @@ nconf.env({
   lowerCase: true,
   parseValues: true
 });
-nconf.defaults(require('./config'));
+nconf.defaults(require('config'));
 const cluster = require('cluster');
+const logger = require('lib/logger');
+// Error handling
+process.on('uncaughtException', function (err) {
+  logger.log({
+    level: 'error',
+    label: 'server',
+    message: err.message
+  });
+});
 if (cluster.isMaster) {
-  const logger = require('lib/logger');
   const shutdown = async function () {
     if (shutdown.executed) return;
     shutdown.executed = true;
@@ -48,14 +56,6 @@ if (cluster.isMaster) {
       cluster.workers[id].send(data);
     }
   });
-  // Error handling
-  process.on('uncaughtException', function (err) {
-    logger.log({
-      level: 'error',
-      label: 'server',
-      message: err.message
-    });
-  });
   // Process termination
   process.once('SIGTERM', shutdown);
   // Ctrl+C
@@ -69,40 +69,19 @@ if (cluster.isMaster) {
   const morgan = require('morgan');
   const compression = require('compression');
   const cors = require('cors');
-  const logger = require('lib/logger');
   const db = require('db');
   const app = express();
-  const createServer = (app) => {
-    const protocol = nconf.get('ssl:key') && nconf.get('ssl:cert')
-      ? 'https'
-      : 'http';
-    const parseConf = (val) => {
-      if (!val) return;
-      try {
-        if (/^-----/.test(val)) {
-          return String(val).replace(/\\n/g, '\n');
-        } else {
-          return fs.readFileSync(val, { encoding: 'utf8' });
-        }
-      } catch (err) {
-        logger.log({
-          level: 'error',
-          label: 'server',
-          message: err.message || err
-        });
-      }
-    };
+  // create server
+  const parseConf = function (val) {
+    if (!val) return;
     try {
-      return protocol === 'https'
-        ? require('https').Server(
-          {
-            key: parseConf(nconf.get('ssl:key')),
-            cert: parseConf(nconf.get('ssl:cert')),
-            ca: parseConf(nconf.get('ssl:ca'))
-          },
-          app
-        )
-        : require('http').Server(app);
+      if (/^-----/.test(val)) {
+        return String(val).replace(/\\n/g, '\n');
+      } else {
+        const data = fs.readFileSync(val, { encoding: 'utf8' });
+        fs.watchFile(val, shutdown);
+        return data;
+      }
     } catch (err) {
       logger.log({
         level: 'error',
@@ -111,135 +90,137 @@ if (cluster.isMaster) {
       });
     }
   };
-  // create server
-  const server = createServer(app);
-  if (server) {
-    // app server
-    app.enable('trust proxy');
-    app.disable('x-powered-by');
-    app.use(
-      morgan(function (tokens, req, res) {
-        const ip = req.ip;
-        const method = tokens.method(req, res);
-        const url = tokens.url(req, res);
-        const statusCode = tokens.status(req, res);
-        const statusMessage = res.statusMessage;
-        const size = tokens.res(req, res, 'content-length') || 0;
-        const time = ~~tokens['response-time'](req, res);
-        const message = `${ip} - ${method} ${url} ${statusCode} (${statusMessage}) ${size} bytes - ${time} ms`;
-        const label = req.protocol;
-        let level;
-        if (res.statusCode >= 100) {
-          level = 'info';
-        } else if (res.statusCode >= 400) {
-          level = 'warn';
-        } else if (res.statusCode >= 500) {
-          level = 'error';
-        } else {
-          level = 'verbose';
-        }
-        logger.log({ level, label, message });
-      })
-    );
-    app.use(compression());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.use(cors({ origin: true }));
-    // routing
-    app.use('/api', require('./routes'));
-    // static
-    [nconf.get('static:dir'), path.join(__dirname, 'public')].forEach(function (dir) {
-      if (!dir) return;
-      app.use(
-        express.static(
-          dir,
-          nconf.get('static:expires')
-            ? { maxAge: nconf.get('static:expires') * 60 * 1000 }
-            : {}
-        )
-      );
-    });
-    // default router
-    app.use(function (req, res, next) {
-      res.status(404);
-      next();
-    });
-    // error handler
-    app.use(function (err, req, res, next) {
-      // fallback to default node handler
-      if (res.headersSent) {
-        return next(err);
-      }
-      // if status not changed
-      if (res.statusCode === 200) {
-        res.status(500);
-      }
-      // convert text to error object
-      if (typeof err !== 'object') {
-        err = new Error(err);
-      }
-      res.json({ name: err.name, message: err.message, code: err.code });
-    });
-    // run server
-    server.once('close', function () {
-      logger.log({
-        level: 'info',
-        label: 'server',
-        message: 'Listener has been stopped'
-      });
-    });
-    server.on('error', function (err) {
-      logger.log({
-        level: 'error',
-        label: 'server',
-        message: err.message || err
-      });
-    });
-    server.listen(nconf.get('port'), nconf.get('host'), function () {
-      const address = this.address();
-      logger.log({
-        level: 'info',
-        label: 'server',
-        message: `Listening on ${address.address}:${address.port}`
-      });
-    });
-    const shutdown = async function () {
-      if (shutdown.executed) return;
-      shutdown.executed = true;
-      const timeout = parseInt(nconf.get('timeout'));
-      if (timeout > 0) {
-        setTimeout(() => process.exit(1), timeout * 1000);
-      }
-      try {
-        await db.disconnect();
-        await Promise.all([
-          new Promise(resolve => server.close(resolve))
-        ]);
-        process.exit(0);
-      } catch (err) {
-        logger.log({
-          level: 'error',
-          label: 'server',
-          message: err.message
-        });
-        process.exit(1);
-      }
-    };
-    // Error handling
-    process.on('uncaughtException', function (err) {
+  const protocol = nconf.get('ssl:key') && nconf.get('ssl:cert')
+    ? 'https'
+    : 'http';
+  const server = (protocol === 'https')
+    ? require('https').Server(
+      {
+        key: parseConf(nconf.get('ssl:key')),
+        cert: parseConf(nconf.get('ssl:cert')),
+        ca: parseConf(nconf.get('ssl:ca'))
+      },
+      app
+    )
+    : require('http').Server(app);
+  // shutdown
+  const shutdown = async function () {
+    if (shutdown.executed) return;
+    shutdown.executed = true;
+    const timeout = parseInt(nconf.get('timeout'));
+    if (timeout > 0) {
+      setTimeout(() => process.exit(1), timeout * 1000);
+    }
+    try {
+      await db.disconnect();
+      await Promise.all([
+        new Promise(resolve => server.close(resolve))
+      ]);
+      process.exit(0);
+    } catch (err) {
       logger.log({
         level: 'error',
         label: 'server',
         message: err.message
       });
+      process.exit(1);
+    }
+  };
+  // app server
+  app.enable('trust proxy');
+  app.disable('x-powered-by');
+  app.use(
+    morgan(function (tokens, req, res) {
+      const ip = req.ip;
+      const method = tokens.method(req, res);
+      const url = tokens.url(req, res);
+      const statusCode = tokens.status(req, res);
+      const statusMessage = res.statusMessage;
+      const size = tokens.res(req, res, 'content-length') || 0;
+      const time = ~~tokens['response-time'](req, res);
+      const message = `${ip} - ${method} ${url} ${statusCode} (${statusMessage}) ${size} bytes - ${time} ms`;
+      const label = req.protocol;
+      let level;
+      if (res.statusCode >= 100) {
+        level = 'info';
+      } else if (res.statusCode >= 400) {
+        level = 'warn';
+      } else if (res.statusCode >= 500) {
+        level = 'error';
+      } else {
+        level = 'verbose';
+      }
+      logger.log({ level, label, message });
+    })
+  );
+  app.use(compression());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors({ origin: true }));
+  // routing
+  app.use('/api', require('./routes'));
+  // static
+  [nconf.get('static:dir'), path.join(__dirname, 'public')].forEach(function (dir) {
+    if (!dir) return;
+    app.use(
+      express.static(
+        dir,
+        nconf.get('static:expires')
+          ? { maxAge: nconf.get('static:expires') * 60 * 1000 }
+          : {}
+      )
+    );
+  });
+  // default router
+  app.use(function (req, res, next) {
+    res.status(404);
+    next();
+  });
+  // error handler
+  app.use(function (err, req, res, next) {
+    // fallback to default node handler
+    if (res.headersSent) {
+      return next(err);
+    }
+    // if status not changed
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+    // convert text to error object
+    if (typeof err !== 'object') {
+      err = new Error(err);
+    }
+    res.json({ name: err.name, message: err.message, code: err.code });
+  });
+  // run server
+  server.once('close', function () {
+    logger.log({
+      level: 'info',
+      label: 'server',
+      message: 'Listener has been stopped'
     });
-    // Process termination
-    process.once('SIGTERM', shutdown);
-    // Ctrl+C
-    process.once('SIGINT', shutdown);
-    // Graceful shutdown for nodemon
-    process.once('SIGUSR2', shutdown);
-    // Connect to DB
-    db.connect();
-  }
+  });
+  server.on('error', function (err) {
+    logger.log({
+      level: 'error',
+      label: 'server',
+      message: err.message || err
+    });
+  });
+  server.listen(nconf.get('port'), nconf.get('host'), function () {
+    const address = this.address();
+    logger.log({
+      level: 'info',
+      label: 'server',
+      message: `Listening on ${address.address}:${address.port}`
+    });
+  });
+  // Process termination
+  process.once('SIGTERM', shutdown);
+  // Ctrl+C
+  process.once('SIGINT', shutdown);
+  // Graceful shutdown for nodemon
+  process.once('SIGUSR2', shutdown);
+  // Connect to DB
+  db.connect();
 }
