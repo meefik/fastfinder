07aaad42c21f7269b36427f0e9a8b8828404a167
mironov-anton <miron.khuzd@gmail.com>
2023-03-05 20:47:04 +0400

    Added parser for partsource

---
+++ src/backend/parsers/partsource.mjs
@@ -0,0 +1,102 @@
+export default async function (page, params, context) {
+  const products = [];
+  let storeAddress = '';
+
+  page.on('dialog', async dialog => {
+    await dialog.accept();
+    await page.evaluate(() => {
+      const parentDiv = document.getElementById('col-main');
+      const currentDiv = document.getElementById('addresses_list');
+      const newDiv = document.createElement('div');
+      newDiv.id = 'notfound';
+      parentDiv.insertBefore(newDiv, currentDiv);
+    });
+  });
+
+  await page.goto('https://www.partsource.ca/apps/store-locator/');
+
+  // Hide ads
+  await page.addStyleTag({ content: '.gf-popupOverlay {display:none!important;}' });
+  await page.addStyleTag({ content: '#m-1635532295580 {display:none!important;}' });
+
+  // Add location by ZIP code (first store found)
+  await page.waitForSelector('#address_search');
+  await page.type('#address_search', params.zip);
+  await page.$eval('#submitBtn', el => el.click());
+  await page.waitForSelector('#notfound, .btnStoreSelect', { hidden: true });
+  if (await page.$('#notfound')) {
+    throw new Error('ZIP not found');
+  }
+  await page.waitForSelector('.btnStoreSelect', { visible: true });
+  await page.$eval('.btnStoreSelect', el => el.click());
+  await page.waitForSelector('.btnStoreSelect[value="Selected"]');
+  const addressSelector = await page.$('#addresses_list li');
+  storeAddress = `${await addressSelector.$eval('.address', el => el.textContent?.trim())}, ${await addressSelector.$eval('.city', el => el.textContent?.trim())}, ${await addressSelector.$eval('.prov_state', el => el.textContent?.trim())} ${await addressSelector.$eval('.postal_zip', el => el.textContent?.trim())}`;
+
+  // Search by part numbers
+  for (const partNumber of params.partNumbers) {
+    await page.goto(`https://www.partsource.ca/search?type=product&q=${encodeURIComponent(partNumber)}*`);
+
+    // Hide ads
+    await page.addStyleTag({ content: '.gf-popupOverlay {display:none!important;}' });
+    await page.addStyleTag({ content: '#m-1635532295580 {display:none!important;}' });
+
+    // Next if no results
+    await page.waitForSelector('span.boost-pfs-filter-total-product', { visible: true });
+    if (await page.$eval('span.boost-pfs-filter-total-product', el => el.childNodes[0].nodeValue.trim()) === '0') {
+      continue;
+    };
+
+    // Read product info from list
+    await page.waitForSelector('div.boost-pfs-filter-product-skeleton-meta', { hidden: true });
+    const productList = await page.$$('div.boost-pfs-filter-product-item-inner');
+    if (productList?.length) {
+      for (let i = 0; i < productList.length; i++) {
+        const productSection = productList[i];
+        const image = await productSection.$eval('img', async el => {
+          el.scrollIntoView();
+          for (let i = 0; i < 10; i++) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+            if (!/^data/.test(el.src)) break;
+          }
+          return el.src;
+        });
+        const title = await productSection.$eval('a.boost-pfs-filter-product-item-title', el => el.textContent?.trim().slice(el.textContent?.trim().indexOf(' ') + 1));
+        const partNumber = await productSection.$eval('a.boost-pfs-filter-product-item-title', el => el.textContent?.trim().slice(0, el.textContent?.trim().indexOf(' ')));
+        let price;
+        if (await productSection.$('span.boost-pfs-filter-product-item-sale-price')) {
+          price = await productSection.$eval('span.boost-pfs-filter-product-item-sale-price', el => {
+            return el.textContent ? parseFloat(el.textContent.replace(/[^0-9.]+/g, '')) : null;
+          });
+        } else {
+          price = await productSection.$eval('span.boost-pfs-filter-product-item-regular-price', el => {
+            return el.textContent ? parseFloat(el.textContent.replace(/[^0-9.]+/g, '')) : null;
+          });
+        }
+        const link = await productSection.$eval('.boost-pfs-filter-product-bottom-inner a', el => el.href);
+
+        // Open new page for product to get availability
+        const productPage = await context.newPage();
+        await productPage.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36');
+        await productPage.goto(link);
+        await productPage.waitForSelector('#part_Availability');
+        const availability = !!(await productPage.$('#part_Availability.partAvailable'));
+        const location = availability ? storeAddress : '';
+        await productPage.close();
+
+        products.push({
+          seller: 'partsource',
+          image,
+          title,
+          partNumber,
+          price,
+          availability,
+          location,
+          link
+        });
+      }
+    }
+  }
+
+  return products;
+};
--- tests/index.mjs
+++ tests/index.mjs
@@ -2,3 +2,5 @@ import './backend.mjs';
 import './frontend.mjs';
 import './parser_autozone.mjs';
 import './parser_oreillyauto.mjs';
+import './parser_advanceautoparts.mjs';
+import './parser_partsource.mjs';
